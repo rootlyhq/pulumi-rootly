937817daec517980f7f367f310750002138af471
Alex Mingoia <alex@alexmingoia.com>
2022-05-19 12:12:06 +0630

    Fix repo name in example.

--- examples/ts/index.ts
+++ examples/ts/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as rootly from "@rootlyhq/pulumi-rootly";
+import * as rootly from "@rootly/pulumi";
 
 new rootly.Severity("sev5", {
 	name: "SEV5",
